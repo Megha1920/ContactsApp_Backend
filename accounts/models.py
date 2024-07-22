f1ea9df7ccaa685ce13be496728c00db043b3b04
Megha <meghacthomas@gmail.com>
2024-07-23 00:29:03 +0530

    authentication correction

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
+
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -5,7 +7,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     date_of_birth = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=10, null=True, blank=True)
-    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    phone_number = models.CharField(max_length=100, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
 
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,18 +7,30 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
     profile_picture = serializers.ImageField(required=False, allow_null=True)
+    phone_numbers = serializers.CharField(write_only=True, required=True)  # Updated to CharField
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name', 'date_of_birth', 'gender', 'phone_number', 'address', 'profile_picture')
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name', 'date_of_birth', 'gender', 'phone_numbers', 'address', 'profile_picture')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        # Validate phone numbers
+        phone_numbers = attrs.get('phone_numbers', '')
+        phone_numbers_list = [num.strip() for num in phone_numbers.split(',')]
+        for phone_number in phone_numbers_list:
+            if not phone_number.isdigit() or len(phone_number) != 10:
+                raise serializers.ValidationError({"phone_numbers": "Each phone number should be 10 digits long."})
+
         return attrs
 
     def create(self, validated_data):
         validated_data.pop('password2')
+        phone_numbers = validated_data.pop('phone_numbers', '')
+        phone_numbers_list = [num.strip() for num in phone_numbers.split(',')]
+
         user = User.objects.create(
             username=validated_data['username'],
             email=validated_data['email'],
@@ -26,12 +38,16 @@ class RegisterSerializer(serializers.ModelSerializer):
             last_name=validated_data['last_name'],
             date_of_birth=validated_data['date_of_birth'],
             gender=validated_data['gender'],
-            phone_number=validated_data['phone_number'],
+            phone_number=phone_numbers_list[0] if phone_numbers_list else '',  # Use the first phone number
             address=validated_data['address'],
             profile_picture=validated_data.get('profile_picture')
         )
         user.set_password(validated_data['password'])
         user.save()
+
+        # Handle additional phone numbers if necessary
+        # For simplicity, we're only saving the first phone number in this example
+
         return user
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
